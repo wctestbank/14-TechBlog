8f8ef35f418be83b0a3e5effdac7f62d45bb2639
wctestbank <wc.businessmail@gmail.com>
2022-05-30 03:53:39 -0400

    added models

---
+++ models/index.js
@@ -0,0 +1,14 @@
+// import models
+const Post = require('./Post');
+const User = require('./User');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User , Post };
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+const sequelize = require("./config/conncection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // handlebars engine
+const hbs = exphbs.create({  });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
